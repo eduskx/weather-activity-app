3474f431ab471702672b4a4bb1f26e7b6b1bc16f
eduskx <91323528+eduskx@users.noreply.github.com>
2024-07-01 14:58:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import { uid } from "uid";
+
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const activities = {
+      id: uid(),
+      name: event.target.name.value,
+      isForGoodWeather: event.target.isForGoodWeather.checked,
+    };
+    onAddActivity(activities);
+    event.target.reset();
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Add new Activity</h2>
+        <label className="name-label" htmlFor="activity-name">
+          Name:{" "}
+        </label>
+        <input id="activity-name" name="name" type="text" required />
+        <br />
+        <label className="checkbox-label" htmlFor="good-weather-activity">
+          Good-weather activity{" "}
+        </label>
+        <input
+          id="good-weather-activity"
+          name="isForGoodWeather"
+          type="checkbox"
+        />
+        <br />
+        <button type="submit" className="button--submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
